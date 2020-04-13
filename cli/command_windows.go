7189aaca2bdffcd9f3d7f9caaf2ae1b11a286c37
bootjp / yoshiaki.ueda <oh@bootjp.me>
2020-04-13 19:47:59 +0900

    fix windows args

--- cli/command_windows.go
+++ cli/command_windows.go
@@ -3,9 +3,17 @@
 package main
 
 import (
+	"os"
 	"os/exec"
+	"syscall"
 )
 
 func command(instance Instance) *exec.Cmd {
-	return exec.Command(runArgs + " " + instance.ID)
+	return &exec.Cmd{
+		Stdin: os.Stdin,
+
+		SysProcAttr: &syscall.SysProcAttr{
+			CmdLine: `/S /C start ` + runArgs + ` ` + instance.ID,
+		},
+	}
 }
