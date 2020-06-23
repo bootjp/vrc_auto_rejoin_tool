625c62d9052a6c92154784343a51ef3e450ac42d
ぶーと / Yoshiaki Ueda <oh@bootjp.me>
2020-06-24 00:28:06 +0900

    [wip]

--- cli/main.go
+++ cli/main.go
@@ -1,12 +1,109 @@
+//Go source to generate custom bundled-fonts.go
+//Replace...)\fyne-io\fyne\theme\bundled-fonts.go by the generated file
 package main
 
 import (
-	"log"
+	"net/url"
 
 	vrcarjt "github.com/bootjp/vrc_auto_rejoin_tool"
+
+	"fyne.io/fyne/theme"
+
+	"fyne.io/fyne"
+	"fyne.io/fyne/canvas"
+	"fyne.io/fyne/layout"
+	"fyne.io/fyne/widget"
+
+	"fyne.io/fyne/app"
 )
 
+var logo = canvas.NewImageFromFile("./logo.png")
+
+//logo.SetMinSize(fyne.NewSize(300, 300))
+
+func parseURL(urlStr string) *url.URL {
+	link, err := url.Parse(urlStr)
+	if err != nil {
+		fyne.LogError("Could not parse URL", err)
+	}
+
+	return link
+}
+
+func help(a fyne.App, vrc *vrcarjt.VRCAutoRejoinTool) fyne.CanvasObject {
+	logo.SetMinSize(fyne.NewSize(300, 300))
+	return widget.NewVBox(
+		layout.NewSpacer(),
+		widget.NewHBox(layout.NewSpacer(), logo, layout.NewSpacer()),
+		widget.NewHBox(layout.NewSpacer(),
+			widget.NewHyperlink("BOOTH", parseURL("https://bootjp.booth.pm/items/1542381")),
+			widget.NewLabel("-"),
+			widget.NewHyperlink("GitHub", parseURL("https://github.com/bootjp/vrc_auto_rejoin_tool")),
+			layout.NewSpacer(),
+		),
+
+		fyne.NewContainerWithLayout(layout.NewCenterLayout(),
+			widget.NewTextGridFromString("version: v.X.X.X"),
+		),
+	)
+}
+
+func welcomeScreen(a fyne.App, v vrcarjt.AutoRejoin) fyne.CanvasObject {
+	return widget.NewVBox(
+		layout.NewSpacer(),
+
+		widget.NewGroup("Controls",
+			fyne.NewContainerWithLayout(layout.NewGridLayout(2),
+				widget.NewButton("Start Sleep this instance", func() {
+					//a.Settings().SetTheme(theme.DarkTheme())
+				}),
+				widget.NewButton("Stop Tool", func() {
+					//a.Settings().SetTheme(theme.LightTheme())
+				}),
+			),
+		),
+	)
+
+}
+
+func settingScreen(a fyne.App, vrc *vrcarjt.VRCAutoRejoinTool) fyne.CanvasObject {
+
+	return widget.NewVBox(
+		layout.NewSpacer(),
+		widget.NewHBox(layout.NewSpacer()),
+		widget.NewGroup("",
+			fyne.NewContainerWithLayout(layout.NewGridLayout(2),
+				widget.NewButton("Save", func() {
+					a.Settings().SetTheme(theme.DarkTheme())
+				}),
+				widget.NewButton("Load setting", func() {
+					a.Settings().SetTheme(theme.DarkTheme())
+				}),
+			),
+		),
+	)
+
+}
+
 func main() {
-	err := vrcarjt.NewVRCAutoRejoinTool().Run()
-	log.Fatal(err)
+	vrc := vrcarjt.NewVRCAutoRejoinTool()
+
+	a := app.NewWithID("vrc_auto_rejoin_tool")
+	a.SetIcon(logo.Resource)
+
+	tabs := widget.NewTabContainer(
+		widget.NewTabItemWithIcon("Control", logo.Resource, welcomeScreen(a, vrc)),
+		widget.NewTabItemWithIcon("Setting", logo.Resource, settingScreen(a, vrc)),
+		widget.NewTabItemWithIcon("About", logo.Resource, help(a, vrc)),
+	)
+
+	//if err := vrc.Run(); err != nil {
+	//	log.Fatal(err)
+	//}
+
+	w := a.NewWindow("VRC AutoRejoinTool")
+	w.SetContent(tabs)
+
+	w.ShowAndRun()
+
 }
--- go.mod
+++ go.mod
@@ -3,10 +3,10 @@ module github.com/bootjp/vrc_auto_rejoin_tool
 go 1.12
 
 require (
+	fyne.io/fyne v1.3.0
 	github.com/StackExchange/wmi v0.0.0-20190523213315-cbe66965904d // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/faiface/beep v1.0.2
-	github.com/fsnotify/fsnotify v1.4.7 // indirect
 	github.com/go-ole/go-ole v1.2.4 // indirect
 	github.com/gofrs/flock v0.7.1
 	github.com/hpcloud/tail v1.0.0
@@ -15,10 +15,8 @@ require (
 	github.com/mitchellh/go-ps v1.0.0
 	github.com/okzk/ticker v0.0.0-20151130031507-5a63c2a83365
 	github.com/shirou/gopsutil v2.20.3+incompatible
-	github.com/stretchr/testify v1.4.0 // indirect
 	golang.org/x/sys v0.0.0-20200409092240-59c9f1ba88fa // indirect
 	golang.org/x/tools v0.0.0-20200504022951-6b6965ac5dd1 // indirect
-	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
 	gopkg.in/fsnotify.v1 v1.4.7 // indirect
 	gopkg.in/tomb.v1 v1.0.0-20141024135613-dd632973f1e7 // indirect
 	gopkg.in/yaml.v2 v2.2.8
