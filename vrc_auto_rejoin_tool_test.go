d1a8c4af07a0fe61161b768d4230d275f8377187
ぶーと / Yoshiaki Ueda <oh@bootjp.me>
2021-02-04 20:33:54 +0900

    add tofu test case

--- vrc_auto_rejoin_tool_test.go
+++ vrc_auto_rejoin_tool_test.go
@@ -31,7 +31,7 @@ func TestParseLatestInstance(t *testing.T) {
 			t.Error(err)
 		}
 
-		content, err := ioutil.ReadFile(d + "/test_log.txt")
+		content, err := ioutil.ReadFile(d + "/.test_data/different_world.txt")
 		if err != nil {
 			t.Error(err)
 		}
@@ -68,7 +68,7 @@ func TestParseLatestInstance(t *testing.T) {
 			t.Error(err)
 		}
 
-		content, err := ioutil.ReadFile(d + "/test_log2.txt")
+		content, err := ioutil.ReadFile(d + "/.test_data/world_reload.txt")
 		if err != nil {
 			t.Error(err)
 		}
@@ -110,6 +110,31 @@ func TestNewInstanceByLog(t *testing.T) {
 	}
 
 	log := `2019.08.18 21:02:38 Log        -  [VRCFlowManagerVRC] Destination set: wrld_cc124ed6-acec-4d55-9866-54ab66af172d`
+	ti, err := time.ParseInLocation(TimeFormat, "2019.08.18 21:02:38", time.Local)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+	expect := Instance{ID: "wrld_cc124ed6-acec-4d55-9866-54ab66af172d", Time: ti}
+	got, err := NewInstanceByLog(log)
+	if err != nil {
+		t.Error(err)
+	}
+	if expect != got {
+		fmt.Printf("%v\n", expect)
+		fmt.Printf("%v\n", got)
+		t.Fatal()
+	}
+}
+
+func TestNewInstanceByLogWithTofu(t *testing.T) {
+	var err error
+	time.Local, err = time.LoadLocation(Location)
+	if err != nil {
+		time.Local = time.FixedZone(Location, 9*60*60)
+	}
+
+	log := `2019.08.18 21:02:38 Log        -  [ǅǅǄǄǅǅǄǅǄǄǄǅǅǅǄǄǅǅǅǅǅǅǅǄǄǄǅǅǅǅǄǅǅǅǄǅǄǄǅǅǄǅǄǅǄǄǄ] Destination set: wrld_cc124ed6-acec-4d55-9866-54ab66af172d`
 	ti, err := time.ParseInLocation(TimeFormat, "2019.08.18 21:02:38", time.Local)
 
 	if err != nil {
@@ -191,6 +216,88 @@ func TestMove(t *testing.T) {
 
 	t.Run("success case", func(t *testing.T) {
 		log := `2019.08.18 21:02:38 Log        -  [VRCFlowManagerVRC] Destination set: wrld_cc124ed6-acec-4d55-9866-54ab66af172d`
+		if err != nil {
+			t.Error(err)
+		}
+		got, err := NewVRCAutoRejoinTool().moved(freeze, log)
+		if err != nil {
+			t.Error(err)
+		}
+
+		if expect != got {
+			fmt.Printf("%v\n", expect)
+			fmt.Printf("%v\n", got)
+			t.FailNow()
+		}
+	})
+
+}
+
+
+func TestMoveWithTofu(t *testing.T) {
+	var err error
+	time.Local, err = time.LoadLocation(Location)
+	if err != nil {
+		time.Local = time.FixedZone(Location, 9*60*60)
+	}
+
+	freeze := time.Date(2018, 1, 1, 0, 0, 0, 0, time.Local)
+
+	ti, err := time.ParseInLocation(TimeFormat, "2019.08.18 21:02:38", time.Local)
+	expect := Instance{ID: "wrld_cc124ed6-acec-4d55-9866-54ab66af172d", Time: ti}
+
+	t.Run("success case", func(t *testing.T) {
+		log := `2019.08.18 21:02:38 Log        -  [ǅǅǄǄǅǅǄǅǄǄǄǅǅǅǄǄǅǅǅǅǅǅǅǄǄǄǅǅǅǅǄǅǅǅǄǅǄǄǅǅǄǅǄǅǄǄǄ] Destination set: wrld_cc124ed6-acec-4d55-9866-54ab66af172d`
+		if err != nil {
+			t.Error(err)
+		}
+		got, err := NewVRCAutoRejoinTool().moved(freeze, log)
+		if err != nil {
+			t.Error(err)
+		}
+
+		if expect != got {
+			fmt.Printf("%v\n", expect)
+			fmt.Printf("%v\n", got)
+			t.FailNow()
+		}
+	})
+
+	t.Run("log not found case", func(t *testing.T) {
+		log := `2019.08.18 21:02:38 Log `
+		if err != nil {
+			t.Error(err)
+		}
+		_, err := NewVRCAutoRejoinTool().moved(freeze, log)
+		if err != ErrNotMoved {
+			t.FailNow()
+		}
+	})
+
+	t.Run("log has nonce", func(t *testing.T) {
+
+		ti, err := time.ParseInLocation(TimeFormat, "2019.08.18 21:48:39", time.Local)
+		expect := Instance{ID: "wrld_58260f57-0076-41d3-a617-c0d0bc8f3d6f:43710~private(usr_d97adcdc-718b-4361-9b75-2c97c0a4993d)~nonce(86CB2A7F4E4AC916CD5A1313F656863C1E80BD2ED63738EA789E2B4C25B48F39)", Time: ti}
+
+		log := `2019.08.18 21:48:39 Log        -  [ǅǅǄǄǅǅǄǅǄǄǄǅǅǅǄǄǅǅǅǅǅǅǅǄǄǄǅǅǅǅǄǅǅǅǄǅǄǄǅǅǄǅǄǅǄǄǄ] Destination set: wrld_58260f57-0076-41d3-a617-c0d0bc8f3d6f:43710~private(usr_d97adcdc-718b-4361-9b75-2c97c0a4993d)~nonce(86CB2A7F4E4AC916CD5A1313F656863C1E80BD2ED63738EA789E2B4C25B48F39)`
+
+		if err != nil {
+			t.Error(err)
+		}
+		got, err := NewVRCAutoRejoinTool().moved(freeze, log)
+		if err != nil {
+			t.Error(err)
+		}
+
+		if expect != got {
+			fmt.Printf("%v\n", expect)
+			fmt.Printf("%v\n", got)
+			t.FailNow()
+		}
+	})
+
+	t.Run("success case", func(t *testing.T) {
+		log := `2019.08.18 21:02:38 Log        -  [ǅǅǄǄǅǅǄǅǄǄǄǅǅǅǄǄǅǅǅǅǅǅǅǄǄǄǅǅǅǅǄǅǅǅǄǅǄǄǅǅǄǅǄǅǄǄǄ] Destination set: wrld_cc124ed6-acec-4d55-9866-54ab66af172d`
 		if err != nil {
 			t.Error(err)
 		}
